0c40edf246774321872f15760206df198fb95e07
Alen Pelin <alen.pelin@gmail.com>
2019-05-10 22:23:38 +1000

    Fix externals undefined issue (josephluck/next-typescript-monorepo#3)

--- blog/next.config.js
+++ blog/next.config.js
@@ -3,11 +3,21 @@ const withCustomBabelConfig = require("next-plugin-custom-babel-config");
 const withTypescript = require("@zeit/next-typescript");
 const withTranspileModules = require("next-plugin-transpile-modules");
 
-module.exports = withCustomBabelConfig(
+// https://github.com/wellcometrust/next-plugin-transpile-modules/issues/11
+function fixExternalsPlugin(config = {}) {
+  const { webpack } = config
+  config.webpack = (config, ...rest) => {
+    config.externals = config.externals || []
+    return webpack(config, ...rest)
+  }
+  return config
+}
+
+module.exports = fixExternalsPlugin(withCustomBabelConfig(
   withTypescript(
     withTranspileModules({
       transpileModules: ["@acme"],
       babelConfigFile: path.resolve("../babel.config.js")
     })
   )
-);
+));
