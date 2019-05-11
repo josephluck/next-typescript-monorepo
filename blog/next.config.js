e6159b3eadace9f24797717dbfc7668ebc2ac012
Alen Pelin <alen.pelin@gmail.com>
2019-05-11 18:23:20 +1000

    Refactor next config

--- blog/next.config.js
+++ blog/next.config.js
@@ -3,21 +3,29 @@ const withCustomBabelConfig = require("next-plugin-custom-babel-config");
 const withTypescript = require("@zeit/next-typescript");
 const withTranspileModules = require("next-plugin-transpile-modules");
 
-// https://github.com/wellcometrust/next-plugin-transpile-modules/issues/11
-function fixExternalsPlugin(config = {}) {
-  const { webpack } = config
+function withCustomWebpack(config = {}) {
+  const { webpack } = config;
+
   config.webpack = (config, ...rest) => {
-    config.externals = config.externals || []
+    
+    // Workaround for issue https://github.com/wellcometrust/next-plugin-transpile-modules/issues/11
+    // TODO: upgrade next-plugin-transpile-modules when the issue is fixed
+    // TODO: remove this workaround when next-plugin-transpile-modules is upgraded
+    config.externals = config.externals || [];
+
     return webpack(config, ...rest)
   }
+
   return config
 }
 
-module.exports = fixExternalsPlugin(withCustomBabelConfig(
-  withTypescript(
-    withTranspileModules({
-      transpileModules: ["@acme"],
-      babelConfigFile: path.resolve("../babel.config.js")
-    })
+module.exports = withCustomWebpack(
+  withCustomBabelConfig(
+    withTypescript(
+      withTranspileModules({
+        transpileModules: ["@acme"],
+        babelConfigFile: path.resolve("../babel.config.js")
+      })
+    )
   )
-));
+);
